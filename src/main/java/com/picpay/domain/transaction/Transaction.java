c333f8d966953856aa7464e955b9b34291f29ebc
Arthur Marcelino <arthurecomp22@gmail.com>
2025-04-02 16:05:19 -0300

    create transaction entity

---
+++ src/main/java/com/picpay/domain/transaction/Transaction.java
@@ -0,0 +1,34 @@
+package com.picpay.domain.transaction;
+
+import com.picpay.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name= "transactions")
+@Table(name= "transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal amount;
+
+    @JoinColumn(name= "sender_id")
+    @ManyToMany
+    private User sender;
+
+    @JoinColumn(name= "receiver_id")
+    @ManyToOne
+    private User receiver;
+
+    private LocalDateTime timestamp;
+}
